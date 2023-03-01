4c0033b09ca0aa47854d31d7c29f221788b4831d
png261 <nhphuong.code@gmail.com>
2023-03-01 23:09:38 +0700

    feat: add Color

---
+++ src/Color.cpp
@@ -0,0 +1,35 @@
+#include "Color.hpp"
+
+Color::Color()
+    : m_red(0)
+    , m_green(0)
+    , m_blue(0)
+    , m_alpha(255)
+{}
+
+Color::Color(uint8_t r, uint8_t g, uint8_t b, uint8_t a)
+    : m_red(r)
+    , m_green(g)
+    , m_blue(b)
+    , m_alpha(a)
+{}
+
+uint8_t Color::r() const
+{
+    return m_red;
+}
+
+uint8_t Color::g() const
+{
+    return m_green;
+}
+
+uint8_t Color::b() const
+{
+    return m_blue;
+}
+
+uint8_t Color::a() const
+{
+    return m_alpha;
+}
---
+++ src/Color.hpp
@@ -0,0 +1,23 @@
+#ifndef COLOR_HPP
+#define COLOR_HPP
+
+#include <stdint.h> // uint8_t
+
+class Color
+{
+public:
+    Color();
+    Color(uint8_t r, uint8_t g, uint8_t b, uint8_t a = 255);
+    uint8_t r() const;
+    uint8_t g() const;
+    uint8_t b() const;
+    uint8_t a() const;
+
+private:
+    uint8_t m_red;
+    uint8_t m_green;
+    uint8_t m_blue;
+    uint8_t m_alpha;
+};
+
+#endif
