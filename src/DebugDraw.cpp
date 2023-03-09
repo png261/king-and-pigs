44e6fc4788e1cbca0292af8e686c614755d2767d
png261 <nhphuong.code@gmail.com>
2023-03-09 14:41:33 +0700

    feat: add all debugDraw functions

--- src/DebugDraw.cpp
+++ src/DebugDraw.cpp
@@ -1,83 +1,114 @@
 #include "DebugDraw.hpp"
-#include <SDL2/SDL_render.h>
-#include <SDL2/SDL_surface.h>
-#include <iostream>
 #include "Camera.hpp"
 #include "Game.hpp"
 #include "SDL.hpp"
+#include "Utils.hpp"
+
+DebugDraw::DebugDraw(Window* window)
+    : m_pWindow(window)
+{
+    m_pRenderer = window->getRenderer();
+    uint32 flags = 0;
+    flags += b2Draw::e_shapeBit;
+    flags += b2Draw::e_jointBit;
+    flags += b2Draw::e_centerOfMassBit;
+    flags += b2Draw::e_aabbBit;
+    flags += b2Draw::e_pairBit;
+    b2Draw::SetFlags(flags);
+}
 
 void DebugDraw::DrawPolygon(const b2Vec2* vertices, int32 vertexCount, const b2Color& color)
 {
-    SDL_Renderer* const renderer = Game::Instance()->getWindow()->getRenderer();
-    // Set the renderer color to the fill color.
-    SDL_SetRenderDrawColor(renderer, color.r * 255, color.g * 255, color.b * 255, color.a * 255);
+    SDL_SetRenderDrawColor(m_pRenderer, color.r * 255, color.g * 255, color.b * 255, color.a * 255);
 
-    // Create an array of SDL points from the Box2D vertices.
     SDL_Point sdlVertices[vertexCount];
     for (int i = 0; i < vertexCount; i++) {
-        b2Vec2 point = Box2D::meterToPixel(vertices[i]) - Camera::Instance()->getPosition();
-        sdlVertices[i] = {static_cast<int>(point.x), static_cast<int>(point.y)};
+        sdlVertices[i] = Utils::b2Vec2ToSDLPoint(
+            Box2D::meterToPixel(vertices[i]) - Camera::Instance()->getPosition());
     }
 
-    // Draw the polygon by calling SDL_RenderDrawLines.
-    // The last point should connect to the first point to close the polygon.
-    SDL_RenderDrawLines(renderer, sdlVertices, vertexCount);
+    SDL_RenderDrawLines(m_pRenderer, sdlVertices, vertexCount);
     SDL_RenderDrawLine(
-        renderer,
+        m_pRenderer,
         sdlVertices[vertexCount - 1].x,
         sdlVertices[vertexCount - 1].y,
         sdlVertices[0].x,
         sdlVertices[0].y);
-
-    // Draw the outline by calling SDL_RenderDrawLines again.
-    SDL_RenderDrawLines(renderer, sdlVertices, vertexCount);
 }
 
 void DebugDraw::DrawSolidPolygon(const b2Vec2* vertices, int32 vertexCount, const b2Color& color)
 {
-    SDL_Renderer* const renderer = Game::Instance()->getWindow()->getRenderer();
-    // Set the renderer color to the fill color.
-    SDL_SetRenderDrawColor(renderer, color.r * 255, color.g * 255, color.b * 255, color.a * 255);
-
-    // Create an array of SDL points from the Box2D vertices.
-    SDL_Point sdlVertices[vertexCount];
-    for (int i = 0; i < vertexCount; i++) {
-        b2Vec2 point = Box2D::meterToPixel(vertices[i]) - Camera::Instance()->getPosition();
-        sdlVertices[i] = {static_cast<int>(point.x), static_cast<int>(point.y)};
-    }
-
-    // Draw the polygon by calling SDL_RenderDrawLines.
-    // The last point should connect to the first point to close the polygon.
-    SDL_RenderDrawLines(renderer, sdlVertices, vertexCount);
-    SDL_RenderDrawLine(
-        renderer,
-        sdlVertices[vertexCount - 1].x,
-        sdlVertices[vertexCount - 1].y,
-        sdlVertices[0].x,
-        sdlVertices[0].y);
-
-    // Draw the outline by calling SDL_RenderDrawLines again.
-    SDL_RenderDrawLines(renderer, sdlVertices, vertexCount);
+    DebugDraw::DrawPolygon(vertices, vertexCount, color);
 }
 
-/// Draw a circle.
-void DebugDraw::DrawCircle(const b2Vec2& center, float radius, const b2Color& color) {}
+void DebugDraw::DrawCircle(const b2Vec2& center, float radius, const b2Color& color)
+{
+    SDL_SetRenderDrawColor(m_pRenderer, color.r * 255, color.g * 255, color.b * 255, color.a * 255);
 
+    const int k_segments = 16;
+    const int vertexCount = k_segments + 1;
 
-/// Draw a solid circle.
+    b2Vec2 vertices[vertexCount];
+
+    float theta = 0.0f;
+    float deltaTheta = 2.0f * b2_pi / float(k_segments);
+
+    for (int32 i = 0; i < k_segments; ++i) {
+        vertices[i] = center + radius * b2Vec2(cosf(theta), sinf(theta));
+        theta += deltaTheta;
+    }
+    vertices[k_segments] = vertices[0];
+
+    SDL_Point sdlVertices[vertexCount];
+    for (int32 i = 0; i < vertexCount; ++i) {
+        sdlVertices[i] = Utils::b2Vec2ToSDLPoint(
+            Box2D::meterToPixel(vertices[i]) - Camera::Instance()->getPosition());
+    }
+
+    SDL_RenderDrawLines(m_pRenderer, sdlVertices, vertexCount);
+}
+
 void DebugDraw::DrawSolidCircle(
     const b2Vec2& center,
     float radius,
     const b2Vec2& axis,
     const b2Color& color)
-{}
+{
+    DebugDraw::DrawCircle(center, radius, color);
+}
 
-/// Draw a line segment.
-void DebugDraw::DrawSegment(const b2Vec2& p1, const b2Vec2& p2, const b2Color& color) {}
+void DebugDraw::DrawSegment(const b2Vec2& p1, const b2Vec2& p2, const b2Color& color)
+{
+    SDL_SetRenderDrawColor(m_pRenderer, color.r * 255, color.g * 255, color.b * 255, color.a * 255);
+    int vertexCount = 2;
+    SDL_Point sdlVertices[vertexCount];
+    sdlVertices[0] =
+        Utils::b2Vec2ToSDLPoint(Box2D::meterToPixel(p1) - Camera::Instance()->getPosition());
+    sdlVertices[1] =
+        Utils::b2Vec2ToSDLPoint(Box2D::meterToPixel(p2) - Camera::Instance()->getPosition());
 
-/// Draw a transform. Choose your own length scale.
-/// @param xf a transform.
-void DebugDraw::DrawTransform(const b2Transform& xf){};
+    SDL_RenderDrawLines(m_pRenderer, sdlVertices, vertexCount);
+}
 
-/// Draw a point.
-void DebugDraw::DrawPoint(const b2Vec2& p, float size, const b2Color& color) {}
+void DebugDraw::DrawTransform(const b2Transform& xf)
+{
+    SDL_Point p1 =
+        Utils::b2Vec2ToSDLPoint(Box2D::meterToPixel(xf.p) - Camera::Instance()->getPosition());
+    SDL_Point p2 = Utils::b2Vec2ToSDLPoint(
+        Box2D::meterToPixel({xf.p.x + xf.q.c * 20.0f, xf.p.y + xf.q.s * 20.0f}) -
+        Camera::Instance()->getPosition());
+    SDL_RenderDrawLine(m_pRenderer, p1.x, p1.y, p2.x, p2.y);
+};
+
+void DebugDraw::DrawPoint(const b2Vec2& p, float size, const b2Color& color)
+{
+    SDL_SetRenderDrawColor(m_pRenderer, color.r * 255, color.g * 255, color.b * 255, color.a * 255);
+    b2Vec2 point = Box2D::meterToPixel(p) - Camera::Instance()->getPosition();
+    size = Box2D::meterToPixel(size);
+    SDL_Rect rect{
+        static_cast<int>(point.x - size / 2.0f),
+        static_cast<int>(point.y - size / 2.0f),
+        static_cast<int>(size),
+        static_cast<int>(size)};
+    SDL_RenderFillRect(m_pRenderer, &rect);
+}
--- src/DebugDraw.hpp
+++ src/DebugDraw.hpp
@@ -1,30 +1,29 @@
 #pragma once
 
 #include "Box2D.hpp"
+#include "Window.hpp"
 
 class DebugDraw final : public b2Draw
 {
 public:
-    /// Draw a closed polygon provided in CCW order.
+    DebugDraw(Window* window);
+
     void DrawPolygon(const b2Vec2* vertices, int32 vertexCount, const b2Color& color);
 
-    /// Draw a solid closed polygon provided in CCW order.
     void DrawSolidPolygon(const b2Vec2* vertices, int32 vertexCount, const b2Color& color);
 
-    /// Draw a circle.
     void DrawCircle(const b2Vec2& center, float radius, const b2Color& color);
 
-    /// Draw a solid circle.
     void
     DrawSolidCircle(const b2Vec2& center, float radius, const b2Vec2& axis, const b2Color& color);
 
-    /// Draw a line segment.
     void DrawSegment(const b2Vec2& p1, const b2Vec2& p2, const b2Color& color);
 
-    /// Draw a transform. Choose your own length scale.
-    /// @param xf a transform.
     void DrawTransform(const b2Transform& xf);
 
-    /// Draw a point.
     void DrawPoint(const b2Vec2& p, float size, const b2Color& color);
+
+private:
+    Window* m_pWindow;
+    SDL_Renderer* m_pRenderer;
 };
