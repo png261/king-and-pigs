629864ca13f79687eeab95753660568a0eff577f
png261 <nhphuong.code@gmail.com>
2023-03-20 14:08:53 +0700

    refactor: add PhysicObject

--- src/GameObject.cpp
+++ src/GameObject.cpp
@@ -5,15 +5,12 @@
 #include "PhysicWorld.hpp"
 
 GameObject::GameObject()
-    : m_width(0)
+    : PhysicObject()
+    , m_width(0)
     , m_height(0)
     , m_bUpdating(false)
     , m_bExist(true)
     , m_bFlipped(false)
-    , m_footContact(0)
-    , m_moveSpeed(0)
-    , m_jumpHeight(0)
-    , m_bOnGround(false)
 {}
 
 GameObject::~GameObject()
@@ -30,39 +27,6 @@ void GameObject::load(std::unique_ptr<LoaderParams> const& pParams)
 
 void GameObject::loadAnimation(){};
 
-void GameObject::createBody(const int x, const int y, const int width, const int height)
-{
-    b2BodyDef bodyDef;
-    bodyDef.type = b2_dynamicBody;
-    bodyDef.position = PhysicWorld::pixelToMeter(b2Vec2(x + width * 0.5f, y + height * 0.5f));
-    bodyDef.fixedRotation = true;
-    bodyDef.userData.pointer = reinterpret_cast<uintptr_t>(this);
-    m_pBody = PhysicWorld::Instance()->getWorld()->CreateBody(&bodyDef);
-
-    b2PolygonShape dynamicBox;
-    dynamicBox.SetAsBox(
-        PhysicWorld::pixelToMeter(width) / 2.0f,
-        PhysicWorld::pixelToMeter(height) / 2.0f);
-
-    b2FixtureDef fixtureDef;
-    fixtureDef.shape = &dynamicBox;
-    fixtureDef.density = 1;
-    fixtureDef.friction = 0.3;
-    m_pFixture = m_pBody->CreateFixture(&fixtureDef);
-
-    dynamicBox.SetAsBox(
-        (PhysicWorld::pixelToMeter(width - 0.5)) / 2.0f,
-        PhysicWorld::pixelToMeter(0.5),
-        b2Vec2(0, PhysicWorld::pixelToMeter(height) / 2.0f),
-        0);
-    b2FixtureDef footSensorDef;
-    footSensorDef.shape = &dynamicBox;
-    footSensorDef.isSensor = true;
-    footSensorDef.filter.categoryBits = PhysicWorld::CAT_FOOT_SENSOR;
-    footSensorDef.filter.maskBits = PhysicWorld::MASK_FOOT_SENSOR;
-    m_pBody->CreateFixture(&footSensorDef);
-}
-
 void GameObject::draw()
 {
     m_animations[m_curAnimation]->draw(
@@ -73,34 +37,11 @@ void GameObject::draw()
 
 void GameObject::update()
 {
-    m_bOnGround = m_footContact > 0;
+    PhysicObject::update();
     m_animations[m_curAnimation]->update();
 }
 
-b2Vec2 GameObject::getPosition() const
-{
-    return PhysicWorld::meterToPixel(m_pBody->GetPosition());
-}
-
-float GameObject::getAngle() const
-{
-    return PhysicWorld::meterToPixel(m_pBody->GetAngle());
-}
-
-int GameObject::getWidth() const
-{
-    return m_width;
-}
-
-int GameObject::getHeight() const
-{
-    return m_height;
-}
-
-b2Body* GameObject::getBody() const
-{
-    return m_pBody;
-}
+void GameObject::updateAnimation() {}
 
 bool GameObject::isUpdating() const
 {
@@ -117,50 +58,17 @@ bool GameObject::isExist() const
     return m_bExist;
 }
 
-void GameObject::changeFootContact(int n)
+void GameObject::disappear()
 {
-    m_footContact += n;
+    m_bExist = false;
 }
 
-void GameObject::moveRight()
+int GameObject::getWidth() const
 {
-    float speedDifference = PhysicWorld::pixelToMeter(m_moveSpeed) - m_pBody->GetLinearVelocity().x;
-    b2Vec2 impulse{m_pBody->GetMass() * speedDifference, 0};
-    m_pBody->ApplyLinearImpulse(impulse, m_pBody->GetWorldCenter(), true);
-
-    if (m_curAnimation != Animation::RUN) {
-        m_animations[m_curAnimation]->stop();
-        m_curAnimation = Animation::RUN;
-        m_animations[m_curAnimation]->start();
-    }
-    m_bFlipped = false;
+    return m_width;
 }
 
-void GameObject::moveLeft()
+int GameObject::getHeight() const
 {
-    float speedDifference =
-        -PhysicWorld::pixelToMeter(m_moveSpeed) - m_pBody->GetLinearVelocity().x;
-    b2Vec2 impulse{m_pBody->GetMass() * speedDifference, 0};
-    m_pBody->ApplyLinearImpulse(impulse, m_pBody->GetWorldCenter(), true);
-
-    if (m_curAnimation != Animation::RUN) {
-        m_animations[m_curAnimation]->stop();
-        m_curAnimation = Animation::RUN;
-        m_animations[m_curAnimation]->start();
-    }
-    m_bFlipped = true;
-}
-
-void GameObject::jump()
-{
-    float timeToJumpPeak =
-        sqrt(2 * PhysicWorld::pixelToMeter(m_jumpHeight) / PhysicWorld::GRAVITY.y);
-    b2Vec2 impulse =
-        -b2Vec2(0, m_pBody->GetMass() * PhysicWorld::pixelToMeter(m_jumpHeight) / timeToJumpPeak);
-    m_pBody->ApplyLinearImpulse(impulse, m_pBody->GetWorldCenter(), true);
-}
-
-bool GameObject::isOnGround() const
-{
-    return m_bOnGround;
+    return m_height;
 }
--- src/GameObject.hpp
+++ src/GameObject.hpp
@@ -3,36 +3,29 @@
 #include <memory>
 #include "Animation.hpp"
 #include "LoaderParams.hpp"
+#include "PhysicObject.hpp"
 #include "PhysicWorld.hpp"
 #include "TextureManager.hpp"
 
-class GameObject
+class GameObject : public PhysicObject
 {
 public:
     virtual ~GameObject();
 
     virtual void load(std::unique_ptr<LoaderParams> const& pParams);
     virtual void update();
+    virtual void updateAnimation();
     virtual void draw();
     virtual void loadAnimation();
-    virtual void createBody(const int x, const int y, const int width, const int height);
 
-    virtual b2Body* getBody() const;
     virtual int getWidth() const;
     virtual int getHeight() const;
-    virtual b2Vec2 getPosition() const;
-    virtual float getAngle() const;
 
     virtual bool isUpdating() const;
     virtual bool isExist() const;
-    virtual bool isOnGround() const;
 
     virtual void setUpdating(const bool bUpdating);
-    virtual void changeFootContact(const int n);
-
-    virtual void moveLeft();
-    virtual void moveRight();
-    virtual void jump();
+    virtual void disappear();
 
 protected:
     GameObject();
@@ -43,15 +36,6 @@ protected:
     bool m_bUpdating;
     bool m_bFlipped;
     bool m_bExist;
-
-    b2Body* m_pBody;
-    b2Fixture* m_pFixture;
-    float m_moveSpeed;
-    float m_jumpHeight;
-
-    int m_footContact;
-
-    bool m_bOnGround;
 
     std::map<Animation::AnimationID, std::unique_ptr<Animation>> m_animations;
     Animation::AnimationID m_curAnimation;
