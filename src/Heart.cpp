234a5595a7a6757de1614af2085cf64d0e279031
png261 <nhphuong.code@gmail.com>
2023-03-23 13:39:17 +0700

    feat: add bonus, update

--- src/Heart.cpp
+++ src/Heart.cpp
@@ -5,21 +5,32 @@
 void Heart::load(std::unique_ptr<LoaderParams> const& pParams)
 {
     ItemObject::load(pParams);
+    m_pBody->SetGravityScale(0);
+    m_pFixture->SetSensor(true);
     this->loadAnimation();
 }
 
 void Heart::loadAnimation()
 {
     m_animations[Animation::IDLE] = std::make_unique<Animation>(Animation("heart idle", 18, 14, 7));
-    m_animations[Animation::HIT] = std::make_unique<Animation>(Animation("heart hit", 18, 14, 2));
+    m_animations[Animation::HIT] =
+        std::make_unique<Animation>(Animation("heart hit", 18, 14, 2, false));
 
     m_curAnimation = Animation::IDLE;
     m_animations[m_curAnimation]->start();
+}
+
+void Heart::update()
+{
+    ItemObject::update();
+    if (m_curAnimation == Animation::HIT && m_animations[m_curAnimation]->isFinished()) {
+        Game::Instance()->getLevel()->getPlayer()->heal(1);
+        this->disappear();
+    }
 }
 
 void Heart::bonus()
 {
     m_curAnimation = Animation::HIT;
     m_animations[m_curAnimation]->start();
-    this->disappear();
 }
--- src/Heart.hpp
+++ src/Heart.hpp
@@ -6,6 +6,7 @@ class Heart final : public ItemObject
 {
 public:
     void load(std::unique_ptr<LoaderParams> const& pParams) override;
+    void update() override;
     void bonus() override;
     void loadAnimation() override;
 };
