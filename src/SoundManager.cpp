8d2cce2e97ccd7fb0f35ea5cf69967c75f5bb78d
png261 <nhphuong.code@gmail.com>
2023-03-28 08:21:19 +0700

    feat(SoundManager): handle volume, support mp3

--- src/SoundManager.cpp
+++ src/SoundManager.cpp
@@ -1,7 +1,5 @@
 #include "SoundManager.hpp"
 
-#include "Log.hpp"
-
 SoundManager* SoundManager::Instance()
 {
     static SoundManager* const s_pInstance = new SoundManager;
@@ -9,8 +7,12 @@ SoundManager* SoundManager::Instance()
 }
 
 SoundManager::SoundManager()
+    : m_bMutedMusic(false)
+    , m_bMutedSFX(false)
+    , m_volume(100)
 {
-    Mix_OpenAudio(22050, AUDIO_S16, 2, (4096 / 2));
+    this->setVolume(100);
+    Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 1024);
 }
 
 SoundManager::~SoundManager()
@@ -18,18 +20,14 @@ SoundManager::~SoundManager()
     Mix_CloseAudio();
 }
 
-bool isValidFileType(const std::string filename, const std::string filetype)
+void SoundManager::clean()
 {
-    return filename.substr(filename.find_last_of(".") + 1) == filetype;
+    m_musicMap.clear();
+    m_sfxMap.clear();
 }
 
 bool SoundManager::loadMusic(const std::string filename, const std::string soundID)
 {
-    if (!isValidFileType(filename, "ogg")) {
-        Log::error("Filetype must be .ogg");
-        return false;
-    }
-
     Mix_Music* const pMusic = Mix_LoadMUS(filename.c_str());
     if (pMusic == nullptr) {
         return false;
@@ -40,11 +38,6 @@ bool SoundManager::loadMusic(const std::string filename, const std::string sound
 
 bool SoundManager::loadSFX(const std::string filename, const std::string soundID)
 {
-    if (!isValidFileType(filename, "wav")) {
-        Log::error("Filetype must be .wav");
-        return false;
-    }
-
     Mix_Chunk* const pSFX = Mix_LoadWAV(filename.c_str());
     if (pSFX == nullptr) {
         return false;
@@ -63,4 +56,83 @@ void SoundManager::playSFX(const std::string soundID, const int loop)
     Mix_PlayChannel(-1, m_sfxMap[soundID], loop);
 }
 
-void SoundManager::clean() {}
+void SoundManager::setVolume(const int percent)
+{
+    m_volume = std::min(100, std::max(0, percent));
+    m_bMutedMusic = false;
+    m_bMutedSFX = false;
+    this->setVolumeMusic(m_volume);
+    this->setVolumeSFX(m_volume);
+}
+
+void SoundManager::changeVolume(const int percent)
+{
+    this->setVolume(m_volume + percent);
+}
+
+void SoundManager::muteMusic()
+{
+    m_bMutedMusic = true;
+    this->setVolumeMusic(0);
+}
+
+void SoundManager::muteSFX()
+{
+    m_bMutedSFX = true;
+    this->setVolumeSFX(0);
+}
+
+void SoundManager::unMuteMusic()
+{
+    m_bMutedMusic = false;
+    this->setVolumeMusic(m_volume);
+}
+
+void SoundManager::unMuteSFX()
+{
+    m_bMutedSFX = false;
+    this->setVolumeSFX(m_volume);
+}
+
+void SoundManager::toggleMuteMusic()
+{
+    if (this->isMutedMusic()) {
+        this->unMuteMusic();
+    } else {
+        this->muteMusic();
+    }
+}
+
+void SoundManager::toggleMuteSFX()
+{
+    if (this->isMutedSFX()) {
+        this->unMuteSFX();
+    } else {
+        this->muteSFX();
+    }
+}
+
+bool SoundManager::isMutedMusic() const
+{
+    return m_bMutedMusic;
+}
+
+bool SoundManager::isMutedSFX() const
+{
+    return m_bMutedSFX;
+}
+
+void SoundManager::setVolumeMusic(const float percent)
+{
+    Mix_VolumeMusic(percent / 100 * MIX_MAX_VOLUME);
+}
+
+void SoundManager::setVolumeSFX(const float percent)
+{
+    Mix_MasterVolume(percent / 100 * MIX_MAX_VOLUME);
+}
+
+int SoundManager::getVolume() const
+{
+    return m_volume;
+}
--- src/SoundManager.hpp
+++ src/SoundManager.hpp
@@ -18,11 +18,31 @@ public:
     void playSFX(const std::string soundID, const int loop = 0);
     void playMusic(const std::string soundID, const int loop = 0);
 
+    void setVolume(const int percent);
+    void changeVolume(const int percent);
+    int getVolume() const;
+
+    void muteSFX();
+    void unMuteSFX();
+    void toggleMuteSFX();
+    bool isMutedSFX() const;
+
+    void muteMusic();
+    void unMuteMusic();
+    void toggleMuteMusic();
+    bool isMutedMusic() const;
+
     void clean();
 
 private:
     SoundManager();
     ~SoundManager();
+    void setVolumeMusic(const float percent);
+    void setVolumeSFX(const float percent);
+
+    bool m_bMutedMusic;
+    bool m_bMutedSFX;
+    float m_volume;
 
     std::map<std::string, Mix_Music*> m_musicMap;
     std::map<std::string, Mix_Chunk*> m_sfxMap;
