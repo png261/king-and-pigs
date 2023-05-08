c0274fad610144eadab04a529fd01bafa9a564ba
png261 <nhphuong.code@gmail.com>
2023-05-08 22:29:03 +0700

    chore: add override

--- src/DebugDraw.hpp
+++ src/DebugDraw.hpp
@@ -8,23 +8,25 @@ class DebugDraw final : public b2Draw
 public:
     DebugDraw(Window* const window);
 
-    void DrawPolygon(const b2Vec2* vertices, const int32 vertexCount, const b2Color& color);
+    void DrawPolygon(const b2Vec2* vertices, const int32 vertexCount, const b2Color& color)
+        override;
 
-    void DrawSolidPolygon(const b2Vec2* vertices, const int32 vertexCount, const b2Color& color);
+    void DrawSolidPolygon(const b2Vec2* vertices, const int32 vertexCount, const b2Color& color)
+        override;
 
-    void DrawCircle(const b2Vec2& center, const float radius, const b2Color& color);
+    void DrawCircle(const b2Vec2& center, const float radius, const b2Color& color) override;
 
     void DrawSolidCircle(
         const b2Vec2& center,
         const float radius,
         const b2Vec2& axis,
-        const b2Color& color);
+        const b2Color& color) override;
 
-    void DrawSegment(const b2Vec2& p1, const b2Vec2& p2, const b2Color& color);
+    void DrawSegment(const b2Vec2& p1, const b2Vec2& p2, const b2Color& color) override;
 
-    void DrawTransform(const b2Transform& xf);
+    void DrawTransform(const b2Transform& xf) override;
 
-    void DrawPoint(const b2Vec2& p, const float size, const b2Color& color);
+    void DrawPoint(const b2Vec2& p, const float size, const b2Color& color) override;
 
 private:
     Window* window_;
